1aadb5b05f033fd79862157b75270a21958842ac
Beverly Pham <beverly.pham@gmail.com>
2019-04-14 11:09:26 -0700

    mad libs and test files are synced

--- test_madlibs.py
+++ test_madlibs.py
@@ -1,9 +1,9 @@
-"""Test file."""
+"""MadLibs test file."""
 
 import pytest
-import madlibs
+from madlibs import *
 
 
-def test():
-    """Test."""
-    pass
+def test_setup_properly():
+    """Test files are synced and welcome function eists."""
+    assert welcome
